b5749e2045f8c6d5cbf0599af460f03e763953f4
samiullahmamu <samiullahudoy@gmail.com>
2024-09-21 20:50:14 +0600

    first commit

---
+++ js/addmoney.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+   event.preventDefault();
+
+   const addMoney =document.getElementById('input-add-money').value;
+   const addMoneyNumber =parseFloat(addMoney);
+   const pinNumber =document.getElementById('input-pin-number').value;
+//    worng way to verfy to pin
+
+if(pinNumber ==='1234'){
+    const balance =document.getElementById('account-balance').innerText;
+    const balanceNumber =parseFloat(balance);
+
+    const newBalance =balanceNumber+addMoneyNumber;
+
+    document.getElementById('account-balance').innerText =newBalance;
+}
+else{
+    alert('Faild to add money.')
+}
+
+})
